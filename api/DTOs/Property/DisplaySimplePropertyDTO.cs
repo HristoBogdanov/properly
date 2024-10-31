6f76a3a35336cf0a167e6b901f653c9196335d80
Hristo Bogdanov <hbogdanov@elevatecompany.eu>
2024-10-31 12:34:18 +0200

    propery DTOs

---
+++ api/DTOs/Property/DisplaySimplePropertyDTO.cs
@@ -0,0 +1,18 @@
+namespace api.DTOs.Property
+{
+    public class DisplaySimplePropertyDTO
+    {
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public decimal Price { get; set; }
+        public bool ForSale { get; set; }
+        public bool ForRent { get; set; }
+        public int Bedrooms { get; set; }
+        public int Bathrooms { get; set; }
+        public bool IsFurnished { get; set; }
+        public int Area { get; set; } = 0;
+        public int YearOfConstruction { get; set; }
+        public Guid OwnerId { get; set; }
+    }
+}
