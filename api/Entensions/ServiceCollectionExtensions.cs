5177b60212b807fac02ced25f8973e17150960c5
Hristo Bogdanov <hbogdanov@elevatecompany.eu>
2024-11-04 22:18:51 +0200

    add extension to automatically inject repositories and services

---
+++ api/Entensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,80 @@
+using System.Reflection;
+using api.Constants;
+using api.Data.Repository;
+using api.Data.Repository.Interfaces;
+using api.Models;
+
+namespace api.Entensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        //Used to automatically register all repositories in the assembly
+        public static void RegisterRepositories(this IServiceCollection services, Assembly modelsAssembly)
+        {
+            // TODO: Re-write the implementation in such way that the user must create a single class for every repository
+            Type[] typesToExclude = new Type[] { typeof(ApplicationUser) };
+            Type[] modelTypes = modelsAssembly
+                .GetTypes()
+                .Where(t => !t.IsAbstract && !t.IsInterface &&
+                            !t.Name.ToLower().EndsWith("attribute"))
+                .ToArray();
+
+            foreach (Type type in modelTypes)
+            {
+                if (!typesToExclude.Contains(type))
+                {
+                    Type repositoryInterface = typeof(IRepository<,>);
+                    Type repositoryInstanceType = typeof(BaseRepository<,>);
+                    PropertyInfo? idPropInfo = type
+                        .GetProperties()
+                        .Where(p => p.Name.ToLower() == "id")
+                        .SingleOrDefault();
+
+                    Type[] constructArgs = new Type[2];
+                    constructArgs[0] = type;
+
+                    if (idPropInfo == null)
+                    {
+                        constructArgs[1] = typeof(object);
+                    }
+                    else
+                    {
+                        constructArgs[1] = idPropInfo.PropertyType;
+                    }
+
+                    repositoryInterface = repositoryInterface.MakeGenericType(constructArgs);
+                    repositoryInstanceType = repositoryInstanceType.MakeGenericType(constructArgs);
+
+                    services.AddScoped(repositoryInterface, repositoryInstanceType);
+                }
+            }
+        }
+
+        //Used to automatically register all services in the assembly
+        public static void RegisterUserDefinedServices(this IServiceCollection services, Assembly serviceAssembly)
+        {
+            Type[] serviceInterfaceTypes = serviceAssembly
+                .GetTypes()
+                .Where(t => t.IsInterface && t.Name.ToLower().EndsWith("service"))
+                .ToArray();
+            Type[] serviceTypes = serviceAssembly
+                .GetTypes()
+                .Where(t => !t.IsInterface 
+                        && !t.IsAbstract 
+                        && t.Name.ToLower().EndsWith("service"))
+                .ToArray();
+
+            foreach (Type serviceInterfaceType in serviceInterfaceTypes)
+            {
+                Type? serviceType = serviceTypes
+                    .SingleOrDefault(t => "i" + t.Name.ToLower() == serviceInterfaceType.Name.ToLower());
+                if (serviceType == null)
+                {
+                    throw new NullReferenceException(CommonErrorMessages.InvalidServiceType + serviceInterfaceType.Name);
+                }
+
+                services.AddScoped(serviceInterfaceType, serviceType);
+            }
+        }
+    }
+}
