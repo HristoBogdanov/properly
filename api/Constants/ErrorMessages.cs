7704b4cd28d26165eac085753f6d6d799d02e682
Hristo Bogdanov <hbogdanov@elevatecompany.eu>
2024-11-04 22:17:05 +0200

    add more error messages

--- api/Constants/ErrorMessages.cs
+++ api/Constants/ErrorMessages.cs
@@ -1,7 +1,6 @@
 namespace api.Constants
 {
-    public static class UserErrorMessages
-    {
+    public static class UserErrorMessages{
         public const string InvalidUsernameLength = "Invalid username length!";
         public const string UsernameRequired = "Username is required!";
         public const string InvalidPasswordLength = "Invalid password length!";
@@ -16,5 +15,10 @@ namespace api.Constants
         public const string TitleRequired = "Title is required!";
         public const string InvalidTitleLength = "Invalid title length!";
         public const string CategoryNotFound = "Category not found!";
+    }
+
+    public static class CommonErrorMessages{
+        public const string EntityNotFound = "Entity not found!";
+        public const string InvalidServiceType = "Service type could not be obtained for the service :";
     }
 }
