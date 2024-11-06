3c489f499d5babbb8a9849a4dbfa3160e7968c37
Hristo Bogdanov <hbogdanov@elevatecompany.eu>
2024-11-06 16:42:17 +0200

    add property error messages

--- api/Constants/ErrorMessages.cs
+++ api/Constants/ErrorMessages.cs
@@ -32,4 +32,24 @@ namespace api.Constants
     public static class ImageErrorMessages{
         public const string ImageAlreadyExists = "Image on this path already exists!";
     }
+
+    public static class PropertiesErrorMessages{
+        public const string PropertyNotFound = "Property not found!";
+        public const string InvalidTitleLength = "Invalid title length!";
+        public const string InvalidPrice = "Invalid price!";
+        public const string InvalidArea = "Invalid area!";
+        public const string InvalidYearOfConstruction = "Invalid year of construction!";
+        public const string InvalidBedrooms = "Invalid number of bedrooms!";
+        public const string InvalidBathrooms = "Invalid number of bathrooms!";
+        public const string InvalidAddressLength = "Invalid address length!";
+        public const string InvalidDescriptionLength = "Invalid description length!";
+        public const string TitleRequired = "Title is required!";
+        public const string AreaRequired = "Area is required!";
+        public const string YearOfConstructionRequired = "Year of construction is required!";
+        public const string PriceRequired = "Price is required!";
+        public const string DescriptionRequired = "Description is required!";
+        public const string AddressRequired = "Address is required!";
+        public const string ForSaleRequired = "Mark the property as for sale or not!";
+        public const string ForRentRequired = "Mark the property as for rent ot not!";
+    }
 }
