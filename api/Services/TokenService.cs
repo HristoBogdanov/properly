25241ccd72b996def26a89ffa2c102c26dc4225e
Hristo Bogdanov <hbogdanov@elevatecompany.eu>
2024-11-07 12:47:37 +0200

    fix jwt to include user role

--- api/Services/TokenService.cs
+++ api/Services/TokenService.cs
@@ -3,6 +3,7 @@ using System.Security.Claims;
 using System.Text;
 using api.Interfaces;
 using api.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
 
 namespace api.Services
@@ -11,37 +12,46 @@ namespace api.Services
     {
         private readonly IConfiguration _configuration;
         private readonly SymmetricSecurityKey _key;
+        private readonly UserManager<ApplicationUser> _userManager;
         
-        public TokenService(IConfiguration config)
+        public TokenService(IConfiguration config, UserManager<ApplicationUser> userManager)
         {
             _configuration = config;
+            _userManager = userManager;
             _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtSettings:DevSigningKey"]!));
         }
 
         public string CreateToken(ApplicationUser user)
-        {
-            var claims = new List<Claim>
-            {
-                new Claim(JwtRegisteredClaimNames.Email, user.Email!),
-                new Claim(JwtRegisteredClaimNames.GivenName, user.UserName!)
-            };
+{
+    var claims = new List<Claim>
+    {
+        new Claim(JwtRegisteredClaimNames.Email, user.Email!),
+        new Claim(JwtRegisteredClaimNames.GivenName, user.UserName!)
+    };
 
-            var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+    // Retrieve the roles for the user
+    var roles = _userManager.GetRolesAsync(user).Result;
+    foreach (var role in roles)
+    {
+        claims.Add(new Claim(ClaimTypes.Role, role));
+    }
 
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Subject = new ClaimsIdentity(claims),
-                Expires = DateTime.Now.AddDays(7),
-                SigningCredentials = creds,
-                Issuer = _configuration["JwtSettings:Issuer"],
-                Audience = _configuration["JwtSettings:Audience"]
-            };
+    var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
 
-            var tokenHandler = new JwtSecurityTokenHandler();
+    var tokenDescriptor = new SecurityTokenDescriptor
+    {
+        Subject = new ClaimsIdentity(claims),
+        Expires = DateTime.Now.AddDays(7),
+        SigningCredentials = creds,
+        Issuer = _configuration["JwtSettings:Issuer"],
+        Audience = _configuration["JwtSettings:Audience"]
+    };
 
-            var token = tokenHandler.CreateToken(tokenDescriptor);
+    var tokenHandler = new JwtSecurityTokenHandler();
 
-            return tokenHandler.WriteToken(token);
-        }
+    var token = tokenHandler.CreateToken(tokenDescriptor);
+
+    return tokenHandler.WriteToken(token);
+}
     }
 }
