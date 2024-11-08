2b2c3d3572db43424de1829a06852315f7c82e8c
Hristo Bogdanov <hbogdanov@elevatecompany.eu>
2024-11-08 15:13:13 +0200

    add get property by id method

--- api/Services/Interfaces/IPropertyService.cs
+++ api/Services/Interfaces/IPropertyService.cs
@@ -1,10 +1,12 @@
 using api.DTOs.Property;
+using api.Helpers;
 
 namespace api.Services.Interfaces
 {
     public interface IPropertyService
     {
-        Task<List<DisplayPropertyDTO>> GetPropertiesAsync();
+        Task<List<DisplayPropertyDTO>> GetPropertiesAsync(PropertyQueryParams queryParams);
+        Task<DisplayPropertyDTO> GetPropertyByIdAsync(string id);
         Task<bool> CreatePropertyAsync(CreatePropertyDTO createPropertyDTO);
         Task<bool> UpdatePropertyAsync(string id, CreatePropertyDTO updatePropertyDTO);
         Task<bool> DeletePropertyAsync(string id);
