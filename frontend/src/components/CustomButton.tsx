0dd4be1f11b59338437a6adaceb8a433d4899b5e
Hristo Bogdanov <hbogdanov@elevatecompany.eu>
2024-11-11 22:38:27 +0200

    fix link not taking whole button width and height bug

--- frontend/src/components/CustomButton.tsx
+++ frontend/src/components/CustomButton.tsx
@@ -22,11 +22,20 @@ export default function CustomButton({
         variant == "primary"
           ? "text-white border-white hover:bg-white hover:text-primary hover:border-primary"
           : "text-primary bg-white border-primary hover:bg-primary hover:text-white hover:border-white"
-      } py-3 px-8 mx-auto w-fit border-2 text-lg md:text-xl font-bold bg-primary rounded-md transition-all duration-300 ease-in-out`}
+      } relative py-3 px-8 mx-auto w-fit border-2 text-lg md:text-xl font-bold bg-primary rounded-md transition-all duration-300 ease-in-out`}
       onClick={onClick}
       {...rest}
     >
-      {link ? <Link to={link}>{text}</Link> : text}
+      {link ? (
+        <Link
+          to={link}
+          className="absolute inset-0 flex items-center justify-center"
+        >
+          {text}
+        </Link>
+      ) : (
+        text
+      )}
     </button>
   );
 }
