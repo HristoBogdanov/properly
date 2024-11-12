83bcc43ee65e49df547fc3027d7a0539841ecc94
Hristo Bogdanov <hbogdanov@elevatecompany.eu>
2024-11-12 10:20:56 +0200

    add server error page

---
+++ frontend/src/pages/ServerErrorPage.tsx
@@ -0,0 +1,3 @@
+export default function ServerErrorPage() {
+  return <div>500 - Internal Server Error</div>;
+}
--- frontend/src/routes/Routes.tsx
+++ frontend/src/routes/Routes.tsx
@@ -3,6 +3,7 @@ import App from "../App";
 import LoginPage from "@/pages/LoginPage";
 import RegisterPage from "@/pages/RegisterPage";
 import HomePage from "@/pages/HomePage";
+import ServerErrorPage from "@/pages/ServerErrorPage";
 
 export const router = createBrowserRouter([
   {
@@ -12,6 +13,7 @@ export const router = createBrowserRouter([
       { path: "", element: <HomePage /> },
       { path: "/register", element: <RegisterPage /> },
       { path: "/login", element: <LoginPage /> },
+      { path: "/server-error", element: <ServerErrorPage /> },
     ],
   },
 ]);
