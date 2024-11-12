18a28fdf2a507dbf2a659cc81460fa9ab9328c47
Hristo Bogdanov <hbogdanov@elevatecompany.eu>
2024-11-12 14:36:31 +0200

    add properties store

---
+++ frontend/src/stores/propertiesStore.ts
@@ -0,0 +1,76 @@
+import { create } from "zustand";
+import { handleError } from "@/helpers/ErrorHandler";
+import {
+  addProperty,
+  getProperties,
+  updateProperty,
+  removeProperty,
+} from "@/api/properties";
+import {
+  CreateProperty,
+  Property,
+  PropertyQueryParams,
+} from "@/types/property";
+
+type PropertiesStore = {
+  properties: Property[];
+  getProperties: (params?: PropertyQueryParams) => Promise<void>;
+  addProperty: (property: CreateProperty) => Promise<void>;
+  updateProperty: (property: CreateProperty, id: string) => Promise<void>;
+  removeProperty: (id: string) => Promise<void>;
+  loading: boolean;
+};
+
+export const usePropertiesStore = create<PropertiesStore>((set) => ({
+  properties: [],
+  loading: true,
+
+  getProperties: async (params?: PropertyQueryParams) => {
+    try {
+      const response = await getProperties(params);
+      if (response) {
+        set({ properties: response.data });
+      }
+    } catch (error) {
+      handleError(error, "Error getting properties");
+    } finally {
+      set({ loading: false });
+    }
+  },
+
+  addProperty: async (property: CreateProperty) => {
+    try {
+      const response = await addProperty(property);
+      if (response?.data) {
+        await usePropertiesStore.getState().getProperties();
+      }
+    } catch (error) {
+      handleError(error, "Error adding property");
+    }
+  },
+
+  updateProperty: async (property: CreateProperty, id: string) => {
+    try {
+      const response = await updateProperty(property, id);
+      if (response?.data) {
+        await usePropertiesStore.getState().getProperties();
+      }
+    } catch (error) {
+      handleError(error, "Error updating property");
+    }
+  },
+
+  removeProperty: async (id: string) => {
+    try {
+      const response = await removeProperty(id);
+      if (response?.data) {
+        await usePropertiesStore.getState().getProperties();
+      }
+    } catch (error) {
+      handleError(error, "Error removing property");
+    }
+  },
+}));
+
+// Initialize the store by calling getProperties
+usePropertiesStore.getState().getProperties();
