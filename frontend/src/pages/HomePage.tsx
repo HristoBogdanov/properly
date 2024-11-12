09a1688ba8028167c936b85b7225d5e9d12afe24
Hristo Bogdanov <hbogdanov@elevatecompany.eu>
2024-11-12 14:36:49 +0200

    add more property types

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,12 +1,3 @@
-import { useImagesStore } from "@/stores/imagesStore";
-
 export default function HomePage() {
-  const images = useImagesStore((state) => state.images);
-  const loading = useImagesStore((state) => state.loading);
-  console.log(images);
-
-  if (loading) {
-    return <div>loading...</div>;
-  }
   return <div>home</div>;
 }
--- frontend/src/types/property.ts
+++ frontend/src/types/property.ts
@@ -23,3 +23,28 @@ export type Property = SimpleProperty & {
   features: Feature[];
   images: CreateImage[];
 };
+
+export type CreateProperty = SimpleProperty & {
+  categories: string[];
+  features: string[];
+  images: CreateImage[];
+}
+
+export type PropertyQueryParams = {
+  search?: string;
+  minPrice?: string;
+  maxPrice?: string;
+  numberOfBedrooms?: string;
+  numberOfBathrooms?: string;
+  minArea?: string;
+  maxArea?: string;
+  minYearOfConstruction?: string;
+  maxYearOfConstruction?: string;
+  forSale?: "true" | "false";
+  forRent?: "true" | "false";
+  isFurnished?: "true" | "false";
+  sortBy?: "price" | "area" | "yearOfConstruction";
+  descending?: "true" | "false";
+  page?: string;
+  perPage?: string;
+};
