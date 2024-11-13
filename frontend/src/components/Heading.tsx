1ee9f957f98306304f665067fc75beb201eeacd3
Hristo Bogdanov <hbogdanov@elevatecompany.eu>
2024-11-13 12:58:57 +0200

    add heading component

---
+++ frontend/src/components/Heading.tsx
@@ -0,0 +1,15 @@
+export default function Heading({ title }: { title: string }) {
+  const [firstPart, secondPart] = title.split(" ");
+
+  return (
+    <div className="w-full flex gap-3 h-[84px]">
+      <div className="w-4 h-full rounded-md bg-primary"></div>
+      <div className="w-full flex flex-col gap-2">
+        <h2 className="text-lg font-semibold uppercase">{firstPart}</h2>
+        <h2 className="text-5xl font-black text-primary uppercase">
+          {secondPart}
+        </h2>
+      </div>
+    </div>
+  );
+}
