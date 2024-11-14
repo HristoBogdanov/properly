a00e2fb1970994a0a017fbb97a44b749eb91aa6b
Hristo Bogdanov <hbogdanov@elevatecompany.eu>
2024-11-14 14:04:45 +0200

    fix navbar location

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -11,7 +11,7 @@ export default function Navbar() {
     <nav
       className={`${
         currentLocation === "/" ? "bg-transparent" : "bg-[#9A9A94]"
-      } w-[100vw] absolute left-1/2 -translate-x-1/2 text-white h-fit py-10 lg:py-16 px-5 md:px-10 z-40`}
+      } w-[100vw] absolute left-1/2 top-0 -translate-x-1/2 text-white h-fit py-10 lg:py-16 px-5 md:px-10 z-40`}
     >
       <div className="absolute inset-0 container my-10 mx-auto flex justify-between items-center text-lg uppercase">
         <div className="w-fit hidden lg:flex gap-10 justify-center items-center">
