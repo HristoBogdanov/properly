7eae5439aeef27856ae78faae798221f7558909b
Hristo Bogdanov <hbogdanov@elevatecompany.eu>
2024-11-14 14:05:39 +0200

    implement simple filters submit logic

--- frontend/src/components/SimpleFilter.tsx
+++ frontend/src/components/SimpleFilter.tsx
@@ -5,6 +5,7 @@ import { z } from "zod";
 import { simpleSeacrhPropertySchema } from "@/lib/schemas";
 import { zodResolver } from "@hookform/resolvers/zod";
 import CustomButton from "./CustomButton";
+import { useNavigate } from "react-router-dom";
 
 type FormData = z.infer<typeof simpleSeacrhPropertySchema>;
 
@@ -12,6 +13,8 @@ export default function SimpleFilter() {
   const methods = useForm<FormData>({
     resolver: zodResolver(simpleSeacrhPropertySchema),
   });
+
+  const navigate = useNavigate();
 
   const sortOptions = [
     { value: "price", label: "Price" },
@@ -25,18 +28,21 @@ export default function SimpleFilter() {
   ];
 
   const onSubmit = (data: FormData) => {
-    console.log("data", data);
+    const filteredData = Object.fromEntries(
+      Object.entries(data).filter(([_, value]) => value !== "") // eslint-disable-line
+    );
+    navigate(`/properties?${new URLSearchParams(filteredData as any).toString()}`);
   };
 
   return (
     <div className="w-fit p-3 border rounded-lg mx-6">
-      <div className="rounded-lg bg-white flex justify-center items-center p-6">
+      <div className="rounded-lg bg-white p-6">
         <FormProvider {...methods}>
           <form
-            className="flex justify-center items-center gap-3 w-fit text-lg text-black"
+            className="flex flex-col lg:flex-row justify-center items-center gap-3 w-fit text-lg text-black"
             onSubmit={methods.handleSubmit(onSubmit)}
           >
-            <Input id="search" classes="border border-[#d5d5d2] rounded-md" />
+            <Input id="search" showError={false} classes="border border-[#d5d5d2] rounded-md" />
             <Dropdown
               id="sortBy"
               classes="border border-[#d5d5d2] rounded-md"
@@ -47,8 +53,9 @@ export default function SimpleFilter() {
               id="descending"
               classes="border border-[#d5d5d2] rounded-md"
               options={descendingOptions}
+              placeholder="Order"
             />
-            <CustomButton text="Search" type="submit" />
+            <CustomButton text="Search" type="submit" classes="max-lg:w-full"/>
           </form>
         </FormProvider>
       </div>
