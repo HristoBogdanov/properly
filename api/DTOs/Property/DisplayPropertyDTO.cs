7adda6750c0091aa5da821ffcc7daccd467bf21b
Hristo Bogdanov <hbogdanov@elevatecompany.eu>
2024-11-14 15:48:19 +0200

    add slug and owner name to properties dtos

--- api/DTOs/Property/DisplayPropertyDTO.cs
+++ api/DTOs/Property/DisplayPropertyDTO.cs
@@ -9,6 +9,7 @@ namespace api.DTOs.Property
         public string Id { get; set; } = null!;
         public string Title { get; set; } = null!;
         public string Description { get; set; } = null!;
+        public string Slug { get; set; } = null!;
         public string Address { get; set; } = null!;
         public decimal Price { get; set; }
         public DateTime CreatedAt { get; set; }
@@ -20,6 +21,7 @@ namespace api.DTOs.Property
         public int Area { get; set; } = 0;
         public int YearOfConstruction { get; set; }
         public string OwnerId { get; set; } = null!;
+        public string OwnerName { get; set; } = null!;
         public virtual IEnumerable<DisplayCategoryDTO> Categories { get; set; } = new List<DisplayCategoryDTO>();
         public virtual IEnumerable<DisplayFeatureDTO> Features { get; set; } = new List<DisplayFeatureDTO>();
         public virtual IEnumerable<CreateImageDTO> Images { get; set; } = new List<CreateImageDTO>();
--- api/DTOs/Property/DisplaySimplePropertyDTO .cs
+++ api/DTOs/Property/DisplaySimplePropertyDTO .cs
@@ -4,11 +4,11 @@ namespace api.DTOs.Property
     {
         public string Id { get; set; } = null!;
         public string Title { get; set; } = null!;
+        public string Slug { get; set; } = null!;
         public string Description { get; set; } = null!;
         public string Address { get; set; } = null!;
         public decimal Price { get; set; }
         public DateTime CreatedAt { get; set; }
-
         public bool ForSale { get; set; }
         public bool ForRent { get; set; }
         public int Bedrooms { get; set; }
@@ -17,5 +17,6 @@ namespace api.DTOs.Property
         public int Area { get; set; } = 0;
         public int YearOfConstruction { get; set; }
         public string OwnerId { get; set; } = null!;
+        public string OwnerName { get; set; } = null!;
     }
 }
