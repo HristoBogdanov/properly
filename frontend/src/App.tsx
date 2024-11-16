867d5950c751a9825b3964771041b4cb125ca999
Hristo Bogdanov <hbogdanov@elevatecompany.eu>
2024-11-16 15:18:40 +0200

    fix heading responsive

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,16 @@ import { ToastContainer } from "react-toastify";
 import { UserProvider } from "./contexts/useAuth";
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/common/Navbar";
+import { SidebarProvider } from "./components/ui/sidebar";
 
 export default function App() {
   return (
     <UserProvider>
-      <Navbar />
-      <Outlet />
-      <ToastContainer />
+      <SidebarProvider>
+        <Navbar />
+        <Outlet />
+        <ToastContainer />
+      </SidebarProvider>
     </UserProvider>
   );
 }
--- frontend/src/components/common/Heading.tsx
+++ frontend/src/components/common/Heading.tsx
@@ -6,7 +6,7 @@ export default function Heading({ title }: { title: string }) {
       <div className="w-4 h-full rounded-md bg-primary"></div>
       <div className="w-full flex flex-col gap-2">
         <h2 className="text-lg font-semibold uppercase">{firstPart}</h2>
-        <h2 className="text-5xl font-black text-primary uppercase">
+        <h2 className="text-4xl lg:text-5xl font-black text-primary uppercase">
           {secondPart}
         </h2>
       </div>
