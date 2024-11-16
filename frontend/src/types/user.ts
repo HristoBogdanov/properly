c56fd301950efcf288af128290e74ec041609eca
Hristo Bogdanov <hbogdanov@elevatecompany.eu>
2024-11-16 12:03:11 +0200

    add more user types

--- frontend/src/types/user.ts
+++ frontend/src/types/user.ts
@@ -8,3 +8,22 @@ export type UserProfile = {
   userName: string;
   email: string;
 };
+
+export type CreateUser = {
+  email: string;
+  username: string;
+  password: string;
+};
+
+export type LoginUser = {
+  username: string;
+  password: string;
+};
+
+export type User = {
+  id: string;
+  username: string;
+  email: string;
+  role: string;
+  numberOfProperties: number;
+};
