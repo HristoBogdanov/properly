184bbce765b2c4f1aa674632cca257abaedbef05
Hristo Bogdanov <hbogdanov@elevatecompany.eu>
2024-11-20 23:42:36 +0200

    fix updatePropertyPage functionality

--- frontend/src/pages/PropertyManager/UpdatePropertyPage.tsx
+++ frontend/src/pages/PropertyManager/UpdatePropertyPage.tsx
@@ -24,40 +24,65 @@ export default function UpdatePropertyPage() {
   const { getPropertyById, loading, updateProperty } = usePropertiesStore();
   const [property, setProperty] = useState<Property | null>(null);
 
-  const { categories, getCategories } = useCategoriesStore();
-  const { features, getFeatures } = useFeaturesStore();
+  const { categories } = useCategoriesStore();
+  const { features } = useFeaturesStore();
 
+  // Set default data to the form, before fetching the property
   const methods = useForm<FormData>({
     resolver: zodResolver(createPropertySchema),
+    defaultValues: {
+      title: "",
+      description: "",
+      address: "",
+      price: 0,
+      bedrooms: 0,
+      bathrooms: 0,
+      area: 0,
+      yearOfConstruction: 0,
+      forSale: false,
+      forRent: false,
+      isFurnished: false,
+      ownerId: "",
+      categories: [],
+      features: [],
+      images: [{ name: "", path: "" }],
+    },
   });
 
+  // After the property is fetched, set the property data to the form
   useEffect(() => {
     const fetchProperty = async () => {
       if (id) {
+        // Fetch the property data by ID
         const fetchedProperty = await getPropertyById(id);
+        // Set the fetched property data to the state
         setProperty(fetchedProperty);
+        // Reset the form values with the fetched property data
+        methods.reset({
+          ...fetchedProperty,
+          // Map the categories to an array of category IDs
+          categories: fetchedProperty.categories.map((c) => c.id),
+          // Map the features to an array of feature IDs
+          features: fetchedProperty.features.map((f) => f.id),
+        });
       }
     };
     fetchProperty();
-  }, [id, getPropertyById]);
-
-  useEffect(() => {
-    getCategories();
-    getFeatures();
-  }, [getCategories, getFeatures]);
+  }, [id, getPropertyById, methods]);
 
   if (!property) {
     return <div>Loading...</div>;
   }
 
   const onSubmit = async (data: FormData) => {
+    console.log(data);
     if (data.forSale === false && data.forRent === false) {
       toast.error("Mark whether the house is for sale, for rent or both!");
       return;
     }
 
     if (!id) {
-      toast.error("Property ID is required");
+      toast.error("There is no such property");
       return;
     }
 
@@ -65,7 +90,7 @@ export default function UpdatePropertyPage() {
       const result = await updateProperty(data, id);
       if (result) {
         toast.success("You have successfully updated the property");
-        navigate("/dashboard/properties");
+        navigate("/dashboard");
       } else {
         toast.error("Error updating property");
       }
@@ -90,7 +115,6 @@ export default function UpdatePropertyPage() {
               isRequired={true}
               errorColor="primary"
               placeholder="Title"
-              defaultValue={property.title}
             />
           </div>
           <div className="flex flex-col gap-2">
@@ -101,7 +125,6 @@ export default function UpdatePropertyPage() {
               isRequired={true}
               errorColor="primary"
               placeholder="Description"
-              defaultValue={property.description}
               classes="min-h-[200px]"
             />
           </div>
@@ -113,7 +136,6 @@ export default function UpdatePropertyPage() {
               isRequired={true}
               errorColor="primary"
               placeholder="Address"
-              defaultValue={property.address}
             />
             <div className="flex flex-col gap-2"></div>
             <p className="text-sm font-semibold">Price</p>
@@ -125,7 +147,6 @@ export default function UpdatePropertyPage() {
               errorColor="primary"
               placeholder="Price"
               valueAsNumber
-              defaultValue={property.price}
             />
           </div>
           <div className="flex flex-col gap-2">
@@ -138,7 +159,6 @@ export default function UpdatePropertyPage() {
               errorColor="primary"
               placeholder="Bedrooms"
               valueAsNumber
-              defaultValue={property.bedrooms}
             />
           </div>
           <div className="flex flex-col gap-2">
@@ -151,7 +171,6 @@ export default function UpdatePropertyPage() {
               errorColor="primary"
               placeholder="Bathrooms"
               valueAsNumber
-              defaultValue={property.bathrooms}
             />
           </div>
           <div className="flex flex-col gap-2">
@@ -164,7 +183,6 @@ export default function UpdatePropertyPage() {
               errorColor="primary"
               placeholder="Area (sq ft)"
               valueAsNumber
-              defaultValue={property.area}
             />
           </div>
           <div className="flex flex-col gap-2">
@@ -177,26 +195,14 @@ export default function UpdatePropertyPage() {
               errorColor="primary"
               placeholder="Year of Construction"
               valueAsNumber
-              defaultValue={property.yearOfConstruction}
             />
           </div>
-          <Checkbox
-            id="forSale"
-            name="forSale"
-            label="For Sale"
-            defaultChecked={property.forSale}
-          />
-          <Checkbox
-            id="forRent"
-            name="forRent"
-            label="For Rent"
-            defaultChecked={property.forRent}
-          />
+          <Checkbox id="forSale" name="forSale" label="For Sale" />
+          <Checkbox id="forRent" name="forRent" label="For Rent" />
           <Checkbox
             id="isFurnished"
             name="isFurnished"
             label="Property is furnished"
-            defaultChecked={property.isFurnished}
           />
 
           <h3 className="text-2xl font-semibold">Categories</h3>
@@ -209,8 +215,7 @@ export default function UpdatePropertyPage() {
                 name="categories"
                 label={category.title}
                 defaultChecked={property.categories.some(
-                  (c) =>
-                    c.id.toLocaleLowerCase() === category.id.toLocaleLowerCase()
+                  (c) => c.id.toLowerCase() === category.id.toLowerCase()
                 )}
                 showError={false}
               />
@@ -226,8 +231,7 @@ export default function UpdatePropertyPage() {
                 name="features"
                 label={feature.title}
                 defaultChecked={property.features.some(
-                  (f) =>
-                    f.id.toLocaleLowerCase() === feature.id.toLocaleLowerCase()
+                  (f) => f.id === feature.id
                 )}
                 showError={false}
               />
