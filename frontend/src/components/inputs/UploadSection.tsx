46088017395b479215b3a53f6ad9a4c308b42bfd
Hristo Bogdanov <hristobogdanov03@pmgkk.com>
2024-11-24 16:32:10 +0200

    add upload section component to be able to reuse it

---
+++ frontend/src/components/inputs/UploadSection.tsx
@@ -0,0 +1,11 @@
+import PreviewImageGrid from "../properties/PreviewImageGrid";
+import Upload from "./Upload/Upload";
+
+export default function UploadSection() {
+  return (
+    <div className="flex flex-col lg:flex-row w-full justify-between gap-10">
+      <Upload type="regular" />
+      <PreviewImageGrid />
+    </div>
+  );
+}
