9823f7525714a562533496ee24caed48b24763d4
Hristo Bogdanov <hristobogdanov03@pmgkk.com>
2024-11-30 17:59:31 +0200

    fix gaps in property description

--- frontend/src/components/properties/PropertyDescription.tsx
+++ frontend/src/components/properties/PropertyDescription.tsx
@@ -1,8 +1,8 @@
 import { Property } from "@/types/property";
 import BathroomsCount from "./BathroomsCount";
 import BedroomsCount from "./BedroomsCount";
-import YearOfConstruction from "./YearOfConstruction";
 import FurnishedTag from "./FurnishedTag";
+import YearOfConstruction from "./YearOfConstruction";
 
 export default function PropertyDescription({
   property,
@@ -28,7 +28,7 @@ export default function PropertyDescription({
       </div>
       <div className="p-10 w-full bg-[#F5F7FB] rounded-lg flex flex-col">
         <h3 className="text-2xl font-semibold pb-4 subheading">Categories</h3>
-        <p className="text-xl flex flex-wrap gap-2 items-center">
+        <p className="text-xl flex flex-wrap gap-6 items-center">
           {property.categories.map((cat) => (
             <p key={cat.id}>{cat.title}</p>
           ))}
@@ -36,11 +36,11 @@ export default function PropertyDescription({
       </div>
       <div className="p-10 w-full bg-[#F5F7FB] rounded-lg flex flex-col">
         <h3 className="text-2xl font-semibold pb-4 subheading">Features</h3>
-        <p className="text-xl flex flex-wrap gap-2 items-center">
+        <p className="text-xl flex flex-wrap gap-6 items-center">
           {property.features.map((feature) => (
             <div
               key={feature.id}
-              className="flex justify-center items-center gap-2"
+              className="flex justify-center items-center gap-1"
             >
               <img
                 src={feature.image.path}
