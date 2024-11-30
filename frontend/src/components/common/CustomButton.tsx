53cfbd29738db0f66c7135bf5f3a24865d79971a
Hristo Bogdanov <hristobogdanov03@pmgkk.com>
2024-11-30 16:54:03 +0200

    fix custom button to work with link

--- frontend/src/components/common/CustomButton.tsx
+++ frontend/src/components/common/CustomButton.tsx
@@ -17,25 +17,34 @@ export default function CustomButton({
   ...rest
 }: ButtonProps) {
   return (
-    <button
-      className={`${classes} ${
-        variant == "primary"
-          ? "text-white border-white hover:bg-white hover:text-primary hover:border-primary"
-          : "text-primary bg-white border-primary hover:bg-primary hover:text-white hover:border-white"
-      } relative py-3 px-8 mx-auto w-fit border-2 text-lg md:text-xl font-bold bg-primary rounded-md transition-all duration-300 ease-in-out`}
-      onClick={onClick}
-      {...rest}
-    >
+    <>
       {link ? (
-        <Link
-          to={link}
-          className="absolute inset-0 flex items-center justify-center"
-        >
-          {text}
+        <Link to={link}>
+          <button
+            className={`${classes} ${
+              variant == "primary"
+                ? "text-white border-white hover:bg-white hover:text-primary hover:border-primary"
+                : "text-primary bg-white border-primary hover:bg-primary hover:text-white hover:border-white"
+            } py-3 px-8 mx-auto w-fit border-2 text-lg md:text-xl font-bold bg-primary rounded-md transition-all duration-300 ease-in-out`}
+            onClick={onClick}
+            {...rest}
+          >
+            {text}
+          </button>
         </Link>
       ) : (
-        text
+        <button
+          className={`${classes} ${
+            variant == "primary"
+              ? "text-white border-white hover:bg-white hover:text-primary hover:border-primary"
+              : "text-primary bg-white border-primary hover:bg-primary hover:text-white hover:border-white"
+          } py-3 px-8 mx-auto w-fit border-2 text-lg md:text-xl font-bold bg-primary rounded-md transition-all duration-300 ease-in-out`}
+          onClick={onClick}
+          {...rest}
+        >
+          {text}
+        </button>
       )}
-    </button>
+    </>
   );
 }
