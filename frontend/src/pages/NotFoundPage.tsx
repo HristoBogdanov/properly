47517489e696e8210d228cc3e4991b2d65c246b3
Hristo Bogdanov <hristobogdanov03@pmgkk.com>
2024-11-30 16:47:38 +0200

    create custom pages for 404 and 500 errors

--- frontend/src/pages/NotFoundPage.tsx
+++ frontend/src/pages/NotFoundPage.tsx
@@ -1,4 +1,6 @@
-export default function ServerErrorPage() {
+import CustomButton from "@/components/common/CustomButton";
+
+export default function NotFoundPage() {
   return (
     <div className="pt-[150px] h-screen container mx-auto flex flex-col items-center justify-evenly px-6">
       <div className="flex flex-col justify-center items-center">
@@ -6,9 +8,10 @@ export default function ServerErrorPage() {
           src="/images/error.png"
           className="w-full max-w-[300px] h-auto mb-10"
         ></img>
-        <p className="text-6xl lg:text-7xl font-black">500</p>
-        <p className="text-2xl lg:text-3xl font-semibold">SERVER ERROR</p>
+        <p className="text-6xl lg:text-7xl font-black">404</p>
+        <p className="text-2xl lg:text-3xl font-semibold">NOT FOUND</p>
       </div>
+      <CustomButton link="/" text="Home" />
     </div>
   );
 }
