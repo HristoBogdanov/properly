8f533ce5dbecd26f76e3682b7b3907b224203575
Hristo Bogdanov <hristobogdanov03@pmgkk.com>
2024-12-10 22:19:02 +0200

    add gallery to property slug page

--- frontend/src/components/properties/PropertyGallery.tsx
+++ frontend/src/components/properties/PropertyGallery.tsx
@@ -1,16 +1,25 @@
 import { Property } from "@/types/property";
 
 export default function PropertyGallery({ property }: { property: Property }) {
+  const coverImage = property.images[0];
+
   return (
     <div className="p-10 w-full bg-[#F5F7FB] rounded-lg flex flex-col">
       <div className="flex aspect-video overflow-hidden">
-        {property.images.map((image) => (
+        <img
+          src={coverImage.path}
+          alt={coverImage.name}
+          className="object-cover opacity-90 hover:opacity-100 hover:scale-110 h-full w-full transition-all duration-500 ease-in-out"
+        />
+      </div>
+      <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {property.images.slice(1).map((image) => (
           <img
-            key={image.path}
+            key={image.name}
             src={image.path}
             alt={image.name}
-            className="object-cover opacity-90 hover:opacity-100 hover:scale-110 h-full w-full transition-all duration-500 ease-in-out"
-          />
+            className="aspect-video w-full object-cover"
+          ></img>
         ))}
       </div>
     </div>
