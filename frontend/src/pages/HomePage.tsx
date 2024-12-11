246a8d7f6fa153f293b253840da97140850e1653
Hristo Bogdanov <hristobogdanov03@pmgkk.com>
2024-12-11 12:12:47 +0200

    refactor and simplify home page

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,56 +1,26 @@
+import CustomButton from "@/components/common/CustomButton";
 import ParralaxContainer from "@/components/common/ParralaxContainer";
 import PropertiesSection from "@/components/properties/PropertiesSection";
 import { usePropertiesStore } from "@/stores/propertiesStore";
-import { Property } from "@/types/property";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 export default function HomePage() {
-  const { loading, getProperties } = usePropertiesStore();
-  const [latestProperties, setLatestProperties] = useState<Property[]>([]);
-  const [budgetProperties, setBudgetProperties] = useState<Property[]>([]);
-  const [highendProperties, setHighendProperties] = useState<Property[]>([]);
+  const { loading, properties, getProperties } = usePropertiesStore();
 
   useEffect(() => {
-    const fetchProperties = async () => {
-      const latestResponse = await getProperties({ page: "1", perPage: "6" });
-      const budgetResponse = await getProperties({
-        maxPrice: "3000000",
-        page: "1",
-        perPage: "6",
-      });
-      const highendResponse = await getProperties({
-        minPrice: "500000",
-        page: "1",
-        perPage: "6",
-      });
-
-      if (latestResponse) setLatestProperties(latestResponse);
-      if (budgetResponse) setBudgetProperties(budgetResponse);
-      if (highendResponse) setHighendProperties(highendResponse);
-    };
-
-    fetchProperties();
+    getProperties({ page: "1", perPage: "12" });
   }, [getProperties]);
 
   return (
     <>
       <ParralaxContainer />
-      <div className="container mx-auto flex flex-col py-10 lg:py-20 justify-center items-center gap-10 lg:gap-20 px-6">
+      <div className="container mx-auto flex flex-col py-10 lg:py-20 justify-center items-end gap-10 px-6">
         <PropertiesSection
           title="Latest Properties"
-          properties={latestProperties}
+          properties={properties}
           loading={loading}
         />
-        <PropertiesSection
-          title="Budget Properties"
-          properties={budgetProperties}
-          loading={loading}
-        />
-        <PropertiesSection
-          title="High-end Properties"
-          properties={highendProperties}
-          loading={loading}
-        />
+        <CustomButton text="Show more" link="/properties" />
       </div>
     </>
   );
